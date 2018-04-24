2f88eb9b80505611d804ad9da0e215ab98796f41
shaun alan <saunlani@gmail.com>
2018-04-23 20:17:51 -0400

    First commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus' Room Detail Generator!\n");
+
+            Start:
+            Console.Write("Enter Length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter Width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter Height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double area;
+            area = (length * width) * 2;
+
+            double perimeter;
+            perimeter = (length * 2) + (width * 2);
+
+
+            double volume;
+            volume = (width * length * height);
+
+            Console.WriteLine("\nArea: " + area);
+            Console.WriteLine("Perimeter: " + perimeter);
+            Console.WriteLine("Volume: " + volume);
+
+            Console.WriteLine("\nContinue? (y/n)");
+            string response;
+            response = Console.ReadLine();
+            if (response == "y")
+            {
+                Console.WriteLine("");
+                goto Start;
+            }
+            else {};
+
+        }
+    }
+}
